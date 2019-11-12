73edc0d5f0436ebb2fe837829524e49e0eabe941
HADMARINE <joonlee031@live.com>
2019-11-12 15:49:03 +0900

    userschema add

--- src/lib/models/User.ts
+++ src/lib/models/User.ts
@@ -1,1 +1,25 @@
 import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  uid: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  enckey: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  data: {
+    type: [Object]
+  }
+});
+
+module.exports = userSchema;
