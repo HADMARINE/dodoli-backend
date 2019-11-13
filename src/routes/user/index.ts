9de2cf3f07df62871f7dd087b1b85f11de6c4987
HADMARINE <joonlee031@live.com>
2019-11-13 09:35:14 +0900

    User/index.ts update

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,14 +1,26 @@
 import express from 'express';
 const router = express.Router();
-
 import bodyParser from 'body-parser';
+import crypto from 'crypto';
+import util from 'util';
+
+// Schema
+import User from '../../lib/models/User';
+
 const throwError = require('../../lib/throwError.js');
 
 router.use(bodyParser.json());
 
-router.get('/', (req: any, res: any) => {
-  const date = new Date();
-  res.send(date);
+router.post('/', async (req: any, res: any) => {
+  const pbkdf2 = util.promisify(crypto.pbkdf2);
+
+  const { id, password } = req.body;
+  const user: any = await User.findOne({ id });
+
+  if (!id || !password) throwError('필수 항목이 입력되지 않았습니다.', 500);
+  if (!user) throwError('로그인에 실패했습니다.', 500);
+
+  const cryptoPassword = await pbkdf2(password, user, 100000, 64, 'sha512');
 });
 
 module.exports = router;
