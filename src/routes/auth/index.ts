ef888d8b0cf679da9a53d569c31fe08a706adc81
HADMARINE <joonlee031@live.com>
2019-11-15 00:42:06 +0900

    update auth (tsfy)

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -6,47 +6,47 @@ import util from 'util';
 import jwt from 'jsonwebtoken';
 
 // Schema
-import User from '../../lib/models/User';
+import User, { UserDocument } from '../../lib/models/User';
 
-const throwError = require('../../lib/throwError.js');
+import throwError from '../../lib/throwError';
 
 router.use(bodyParser.json());
 
 router.post('/', async (req: any, res: any, next: any) => {
   try {
-    const pbkdf2 = util.promisify(crypto.pbkdf2);
+    const pbkdf2: Function = util.promisify(crypto.pbkdf2);
 
     const { id, password } = req.body;
-    const user: any = await User.findOne({ id });
+    const user: any = User.findOne({ id });
 
     if (!id || !password) throwError('필수 항목이 입력되지 않았습니다.', 500);
     if (!user) throwError('로그인에 실패했습니다.', 500);
 
-    const cryptoPassword = await pbkdf2(
+    const cryptoPassword: string = pbkdf2(
       password,
       user.enckey,
       100000,
       64,
       'sha512'
-    );
+    ).toString();
 
     if (user.password !== cryptoPassword) {
       throwError('로그인에 실패했습니다.', 500);
     }
 
-    const payload = {
+    const payload: object = {
       userId: user.uid,
       nickname: user.nickname,
       _id: user._id
     };
-    const tokenExpireTime = Date.now() + 10800;
+    const tokenExpireTime: number = Date.now() + 10800;
 
-    const jwtSettings = {
+    const jwtSettings: object = {
       expiresIn: tokenExpireTime,
       issuer: 'dodoli.net'
     };
 
-    const result = jwt.sign(
+    const result: string = jwt.sign(
       payload,
       process.env.TOKEN_KEY || 'tokenkey',
       jwtSettings
@@ -57,4 +57,4 @@ router.post('/', async (req: any, res: any, next: any) => {
   }
 });
 
-module.exports = router;
+export default router;
