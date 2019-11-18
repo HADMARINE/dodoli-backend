99104916dc07970e2ed503456d9c8de5d8babad8
HADMARINE <joonlee031@live.com>
2019-11-18 21:16:25 +0900

    test..

--- app.ts
+++ app.ts
@@ -11,7 +11,7 @@ const routes = getRoutes();
 
 app.use(
   cors({
-    origin: process.env.NODE_ENV === 'development' ? '*' : 'https://dodoli.net'
+    origin: process.env.NODE_ENV === 'development' ? '*' : '*'
   })
 );
 app.use(bodyParser.json({ extended: true }));
