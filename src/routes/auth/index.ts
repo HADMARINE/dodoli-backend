28c1fe797b50317beedfcd9736b4caa306355116
HADMARINE <joonlee031@live.com>
2019-11-18 10:08:51 +0900

    update : status change

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -18,19 +18,19 @@ router.post('/', async (req: any, res: any, next: any) => {
 
     const { uid, password, publicip } = req.body;
     if (!uid || !password || !publicip) {
-      throwError('필수 항목이 입력되지 않았습니다.', 500);
+      throwError('필수 항목이 입력되지 않았습니다.', 400);
     }
 
     // tslint:disable-next-line: await-promise
     const user: any = await User.findOne({ uid });
-    if (!user) throwError('로그인에 실패했습니다.', 500);
+    if (!user) throwError('로그인에 실패했습니다.', 403);
 
     const cryptoPassword: string = (
       await pbkdf2(password, user.enckey, 100000, 64, 'sha512')
     ).toString('base64');
 
     if (user.password !== cryptoPassword) {
-      throwError('로그인에 실패했습니다.', 500);
+      throwError('로그인에 실패했습니다.', 403);
     }
 
     const payload: object = {
@@ -60,9 +60,9 @@ router.post('/', async (req: any, res: any, next: any) => {
 router.post('/verify', async (req, res, next) => {
   try {
     const token: any = req.headers['x-access-token'];
-    const userPublicIp = req.body.publicip;
+    const userPublicIp: any = req.body.publicip;
 
-    if (!token) {
+    if (!token || !userPublicIp) {
       return throwError('필수 항목이 입력되지 않았습니다.', 400);
     }
 
@@ -75,8 +75,7 @@ router.post('/verify', async (req, res, next) => {
     } catch (e) {
       return throwError('토큰 검증에 실패했습니다.', 403);
     }
-
-    return tokenValue.userid;
+    res.status(200).json({ userid: tokenValue.userId });
   } catch (e) {
     next(e);
   }
